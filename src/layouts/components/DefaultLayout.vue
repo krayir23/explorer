2bc3ff4c64204e21862d970dace23771fd4e3957
Krayir <krayir@>
2024-10-29 15:58:17 +0300

    logo fix

--- src/layouts/components/DefaultLayout.vue
+++ src/layouts/components/DefaultLayout.vue
@@ -81,7 +81,7 @@ dayjs()
     >
       <div class="flex justify-between mt-1 pl-4 py-4 mb-1">
         <RouterLink to="/" class="flex items-center">
-          <img class="w-10 h-10" src="../../assets/logo.png" />
+          <img class="w-10 h-10" src="../../assets/logo.svg" />
           <h1 class="flex-1 ml-3 text-2xl font-semibold dark:text-white">
             KrayirNode
           </h1>
